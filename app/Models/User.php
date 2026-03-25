adf461f3dc49983dd96c10f410dbbd96e0c1cdb3
Andrei Ciungulete <andrei.ciungulete@andali.ro>
2026-03-25 20:19:57 +0200

    fix: allow second user to access admin panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -34,6 +34,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return $this->id === 1;
+        return $this->id === 1 || $this->id === 2;
     }
 }
